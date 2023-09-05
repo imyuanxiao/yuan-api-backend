f52835840c1f807876392632a7224a020b1116c8
imyuanxiao <imyuanxiao@126.com>
2023-09-05 14:57:03 +0800

    为模拟接口增加测试用接口

--- yuan-api-interface/src/main/java/com/imyuanxiao/yuanapiinterface/controller/InnerApiController.java
+++ yuan-api-interface/src/main/java/com/imyuanxiao/yuanapiinterface/controller/InnerApiController.java
@@ -1,7 +1,12 @@
 package com.imyuanxiao.yuanapiinterface.controller;
 
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.URLUtil;
 import io.swagger.annotations.Api;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "内部接口")
 public class InnerApiController {
 
-
+    @GetMapping("/getName")
+    public String getUsername(String name, HttpServletRequest request) {
+        System.out.println(name);
+        return "你的名字是：" + name;
+    }
 
 }
